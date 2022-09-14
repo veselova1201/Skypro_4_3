0c48d057f3e3484e0513afec2f073b98505de626
ElenA <vselova1201@yandx.ru>
2022-09-14 21:52:51 +0700

    First 3 commit

---
+++ src/Main.java
@@ -0,0 +1,328 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //задание 1
+
+        //
+        int age = 20;
+        if (age >= 18)
+        {
+            int age1 = 20;
+            if (age >= 18)
+            {
+
+                System.out.println("поздравляю с соверннолетием");
+            } else
+            {
+                System.out.println("надо немного подождать");
+            }
+            //задание 2
+
+            //
+            int age2 = 70;
+            if (age2 >= 7 && age2 < 18)
+            {
+                System.out.println("ребенок ходит в школу");
+            } else if (age2 >= 18 && age2 >= 24)
+            {
+                System.out.println("Человек пошел в институт");
+            }
+            if (age2 >= 24)
+            {
+                System.out.println("Человеку пора на работу");
+            }
+            //  задание 3
+            //
+            int carСapacityOFHumans = 102;
+            int seats = 60;
+            if (seats < 60 && seats <= 102)
+            {
+
+                int age3 = 70;
+                if (age2 >= 7 && age2 < 18)
+                {
+                    System.out.println("ребенок ходит в школу");
+                } else if (age2 >= 18 && age2 >= 24) {
+                    System.out.println("Человек пошел в институт");
+                }
+                if (age2 >= 24)
+                {
+                    System.out.println("Человеку пора на работу");
+                }
+                //  задание 3
+                //
+                int carСapacityOFHumans1 = 102;
+                int seats2 = 60;
+                if (seats < 60 && seats <= 102) {
+                    System.out.println("Сидячих мест нет ");
+                } else if
+                (carСapacityOFHumans >= 102 & seats >= 60) ;
+                System.out.println("Сидячие места еще есть");
+                //заданиме 3
+
+                //
+                if (age2 >= 18)
+                {
+                    System.out.println("Поздравляю с совершеннолетием");
+                    int ag2 = 20;
+                    if (age2 >= 18)
+                    {
+                        System.out.println("Нужно немного подождать");
+                        // задание 4
+                        //
+                        int carСapacityOFHumans2 = 102;
+                        int seats3 = 60;
+                        if (seats < 60 && seats <= 102)
+                        {
+
+                            if (age2 >= 18)
+                            {
+                                System.out.println("Поздравляю с совершеннолетием");
+                                int age4 = 20;
+                                if (age2 >= 18)
+                                {
+                                    System.out.println("Нужно немного подождать");
+                                    // задание 4
+                                    //
+                                    int carСapacityOFHumans3 = 102;
+                                    int seats4 = 60;
+                                    if (seats < 60 && seats <= 102) {
+
+                                        System.out.println("Сидячих мест нет ");
+                                    } else if (carСapacityOFHumans >= 102 & seats >= 60) ;
+                                    System.out.println("Сидячие места еще есть");
+                                    //задание 5
+
+                                    //
+                                    int age5 = 7;
+                                    if (age2 >= 2) {
+                                        if (age2 < 6) {
+
+                                            int age6 = 7;
+                                            if (age2 >= 2)
+                                            {
+                                                if (age2 < 6)
+                                                {
+
+                                                }
+                                                System.out.println("Если возраст человека равен 2 годам, то ему нужно ходить в детский сад");
+                                                int age7 = 20;
+                                                if (age3 >= 7) ;
+
+
+                                                if (age3 < 18)
+                                                {
+                                                    System.out.println("Если возраст человека равен 7 годам, то ему нужно ходить в школу");
+                                                    if (age2 >= 18)
+                                                    {
+                                                        if (age < 24)
+                                                        {
+                                                            System.out.println("Если возраст человека равен 18 годам, то ему нужно ходить университет");
+                                                            if (age4 >= 24)
+                                                            {
+
+
+                                                                if (age3 < 18)
+                                                                {
+                                                                    System.out.println("Если возраст человека равен 7 годам, то ему нужно ходить в школу");
+                                                                    if (age2 >= 18)
+                                                                    {
+                                                                        if (age < 24)
+                                                                        {
+                                                                            System.out.println("Если возраст человека равен 18 годам, то ему нужно ходить университет");
+                                                                            if (age4 >= 24)
+                                                                            {
+
+
+                                                                            }
+                                                                            System.out.println("Если возраст человеку равен 24 годам, то ему нужно ходить на работу");
+                                                                        }
+                                                                        //задание 6
+
+
+                                                                        //
+
+                                                                        int age8 = 5;
+                                                                        if (age6 > 2 && age6 <= 5) {
+                                                                            {
+                                                                                {
+                                                                                    System.out.println("Если возраст ребенка меньше 5 лет, то он не может кататься на аттракционах");
+                                                                                }
+
+
+                                                                                if (age2 > 5)
+                                                                                {
+                                                                                    if (age3 <= 14)
+                                                                                    {
+                                                                                        System.out.println("Если возраст ребенка больше 5 ,но меньше 14 ,то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельз");
+                                                                                        if (age2 >= 14)
+                                                                                        {
+                                                                                            System.out.println("Если возраст ребенка больше 14 лет, то он может кататься один");
+
+
+                                                                                            if (1 < 3)
+                                                                                            {
+                                                                                                if (2 < 3)
+                                                                                                {
+                                                                                                    if (3 >= 3)
+                                                                                                    {
+
+                                                                                                        if (age2 > 5)
+                                                                                                        {
+                                                                                                            if (age3 <= 14)
+                                                                                                            {
+                                                                                                                System.out.println("Если возраст ребенка больше 5 ,но меньше 14 ,то он может кататься только в сопровождении взрослого. Если взрослого нет, то кататься нельз");
+                                                                                                                if (age2 >= 14)
+                                                                                                                {
+                                                                                                                    System.out.println("Если возраст ребенка больше 14 лет, то он может кататься один");
+
+
+                                                                                                                    if (1 < 3)
+                                                                                                                    {
+                                                                                                                        if (2 < 3)
+                                                                                                                        {
+                                                                                                                            if (3 >= 3)
+                                                                                                                            {
+
+                                                                                                                                System.out.println("Три больше всех трех чисел");
+
+
+
+                                                                                                                            }
+
+
+                                                                                                                        }
+
+
+                                                                                                                    }
+                                                                                                                }
+
+                                                                                                            }
+
+                                                                                                        }
+                                                                                                    }
+                                                                                                }
+                                                                                            }
+                                                                                        }
+                                                                                    }
+                                                                                }
+
+                                                                            }
+                                                                        }
+
+                                                                    }
+                                                                }
+                                                            }
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
